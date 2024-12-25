a31c91578716e089a718ad99319e1699fd7afc7e
hustle2live <kuz.v@ukr.net>
2024-12-25 11:58:13 +0200

    feat: set up websockets connections

---
+++ server/config.js
@@ -0,0 +1,15 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import dotenv from 'dotenv';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const STATIC_PATH = path.join(__dirname, 'public');
+export const HTML_FILES_PATH = path.join(STATIC_PATH, 'html');
+
+dotenv.config();
+
+export const PORT = process.env.PORT || 3001;
+export const DATABASE_URL = process.env.ATLAS_URI;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,19 +1,19 @@
+import path from 'node:path';
+import { HTML_FILES_PATH } from '../config.js';
+
 import userRouter from './api/userRouter.js';
 import messageRouter from './api/messageRouter.js';
 import chatRouter from './api/chatRouter.js';
 
-function AppRouter(app) {
-   return {
-      init: () => {
-         app.get('/api', (req, res) => {
-            console.log('API running');
-            res.status(201).send('Hello API');
-         });
-         app.use('/api/users', userRouter);
-         app.use('/api/messages', messageRouter);
-         app.use('/api/chats', chatRouter);
-      }
-   };
-}
+const router = (app) => {
+   app.get('/', (_, res) => {
+      const page = path.join(HTML_FILES_PATH, 'index.html');
+      res.sendFile(page);
+   });
+   app.get('/api', (_, res) => res.status(200).send(`<p>Server is running..</p>`));
+   app.use('/api/users', userRouter);
+   app.use('/api/messages', messageRouter);
+   app.use('/api/chats', chatRouter);
+};
 
-export default AppRouter;
+export default router;
